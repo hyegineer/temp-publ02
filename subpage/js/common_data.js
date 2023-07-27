03dc85d8268f04d33dc3348e807e58a3bfdf9030
zsj <shanji920125@gmail.com>
2023-07-27 16:01:52 +0900

    nmf_gene 추가

---
+++ subpage/js/common_data.js
@@ -0,0 +1,24 @@
+var nmfMetaData = ['Subtype','Subtype Core','Subtype Membership','Histology','Age','Sex','Smoking','TNM stage','Pathologic-N',
+'Adjuvant Treatment','Recurrence Status','TIL pattern','Immune Cluster','Whole Genome Doubling','TP53','Other Tumor Suppressor genes',
+'EGFR','Other Oncogene Alteration']
+
+var nmfMetaDataJson = {
+    "Subtype":[1,2,3,4,5],
+    "Subtype Core":['N','Y'],
+    "Subtype Membership":[],
+    "Histology":['AD','MA','NC','SC','Others'],
+    "Age":[],
+    "Sex":[],
+    "Smoking":[],
+    "TNM stage":[],
+    "Pathologic-N":[],
+    "Adjuvant Treatment":[],
+    "Recurrence Status":[],
+    "TIL pattern":[],
+    "Immune Cluster":[],
+    "Whole Genome Doubling":[],
+    "TP53":[],
+    "Other Tumor Suppressor genes":[],
+    "EGFR":[],
+    "Other Oncogene Alteration":[]
+}
