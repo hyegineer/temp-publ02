069e38db87ae41abb0023db66d915714d0b7943c
SUN YU <suny71327@SUNdeMacBook-Air.local>
2023-09-05 14:58:43 +0900

    quantitation fix

--- subpage/js/common_data.js
+++ subpage/js/common_data.js
@@ -113,3 +113,24 @@ var toolbarMenu = {
         'Find Action', null, 'Contact', 'Configuration', 'Tutorial', 'Source Code', null, 'Keyboard' +
         ' Shortcuts']*/
   }
+
+
+var metaArray=[];
+
+var color = [
+  "#70DB93",
+  "#5C3317",
+  "#9F5F9F",
+  "#B5A642",
+  "#8C7853",
+  "#A67D3D",
+  "#5F9F9F",
+  "#D98719",
+  "#B87333",
+];
+
+
+class PatientMetaData {
+  constructor() {
+  }
+}
